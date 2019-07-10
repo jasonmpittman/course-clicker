d34bdb66057be772e5eacfc35830e347b2ee60d1
jasonmpittman <jpittman@cs.highpoint.edu>
2019-07-10 11:39:32 -0400

    updated core module and data model. added html/css template and content

--- course-clicker.py
+++ course-clicker.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import (
+    Flask, render_template, request, flash, redirect, url_for, session
+)
 from models import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 clicker = Flask(__name__)
 
@@ -9,10 +12,57 @@ clicker.config.from_object('config')
 db.init_app(clicker)
 db.create_all(app=clicker)
 
+# route for home
 @clicker.route('/')
-
 def home():
-    return 'hello world'
+    return render_template('index.html')
+
+# route for login handling
+@clicker.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    user = Users.query.filter_by(username=username).first()
+
+    if user:
+        password_hash = user.password
+
+        if check_password_hash(password_hash, password):
+            session['user'] = username
+            flash('Login successful')
+    else:
+        flash('Username or password is incorrect')
+    
+    return redirect(url_for('home'))
+
+# route for logout handling
+@clicker.route('/logout')
+def logout():
+    if 'user' in session:
+        session.pop('user')
+        flash('Logout successful')
+    
+    return redirect(url_for('home'))
+
+# route for signup handling
+@clicker.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+    
+        password = generate_password_hash(password)
+
+        user = Users(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Signup complete. Please login to continue.')
+
+        return redirect(url_for('home'))
+    
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     clicker.run()
--- models.py
+++ models.py
@@ -11,15 +11,18 @@ class Base(db.Model):
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
         onupdate=db.func.current_timestamp())
 
+# question data model
 class Questions(Base):
     question = db.Column(db.String(5000))
 
     def __repr__(self):
         return self.question
 
+# answer data model
 class Answers(Base):
     answer = db.Column(db.String(500))
 
+# poll (question + answer choices) data model
 class Polls(Base):
     question_id = db.Column(db.Integer, db.ForeignKey('questions.id'))
     answer_id = db.Column(db.Integer, db.ForeignKey('answers.id'))
@@ -34,3 +37,7 @@ class Polls(Base):
 
     def __repr__(self):
         return self.answer.name
+
+class Users(Base):
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(200))
